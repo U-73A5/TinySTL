b960a282b19a603c863ae89ed1bfd5fba5d4b624
U-73A5 <934997459@qq.com>
2022-01-01 10:04:19 +0800

    空间配置器

--- TinySTL/alloc.h
+++ TinySTL/alloc.h
@@ -46,6 +46,7 @@ template <class T, class Alloc>
     }
   };
 
+
 /**
  * 第一级配置器
  */
@@ -53,7 +54,7 @@ template <class T, class Alloc>
 #   include <new>
 #   define __THROW_BAD_ALLOC throw std::bad_alloc;
 #elif !defined(__THROW_BAD_ALLOC)
-#   include <iostream.h>
+#   include <iostream>
 #   define __THROW_BAD_ALLOC \
            std::cerr << "out of memory" << endl; \
            exit(1);
@@ -133,6 +134,7 @@ template <int inst>
     }
   }
 
+
 /**
  * 第二级配置器
  * 区块超过 128 bytes 移交第一级配置器，否则以内存池(memory pool)管理。
@@ -144,15 +146,16 @@ template <int inst>
 
 enum
 {
-  __ALIGN = 8; // 小型区块的上调边界
-  __MAX_BYTES = 128; // 小型区块的上限
-  __NFREELISTS = __MAX_BYTES / __ALIGN; // free-lists 个数
+  __ALIGN = 8, // 小型区块的上调边界
+  __MAX_BYTES = 128, // 小型区块的上限
+  __NFREELISTS = __MAX_BYTES / __ALIGN // free-lists 个数
 };
 
 // 第一参数用于多线程环境下，本练习不考虑
 template <bool threads, int inst>
   class __default_alloc_template
   {
+    /* Private */
     private:
     // ROUNND_UP() 将 bytes 上调至 8 的倍数
     static size_t ROUND_UP(size_t bytes)
@@ -167,7 +170,7 @@ template <bool threads, int inst>
     {
       union obj* free_list_link;
       char client_data[1];
-    }
+    };
 
     // 16 个 free-lists
     static obj* volatile free_list[__NFREELISTS];
@@ -190,11 +193,48 @@ template <bool threads, int inst>
     static char* end_free;  // 内存池结束位置
     static size_t heap_size;
 
+    /* Public */
     public:
-    static void* allocate(size_t n);
-    static void deallocate(void* p, size_t n);
+    // n must be > 0
+    static void* allocate(size_t n)
+    {
+      obj* volatile* my_free_list;
+      obj* result;
+      // 大于 128 调用第一级配置器
+      if (n > (size_t)__MAX_BYTES) {
+        return malloc_alloc::allocate(n);
+      }
+      // 寻找 16 个自由链表中合适的一个
+      my_free_list = free_list + FREELIST_INDEX(n);
+      result = *my_free_list;
+      if (0 == result) {
+        // 没有可用链表，重新扩充相应大小的链表
+        void* r = refill(ROUND_UP(n));
+        return r;
+      }
+      // 调整链表
+      *my_free_list = result -> free_list_link;
+      return result;
+    }
+
+    // p 不能为 0
+    static void deallocate(void* p, size_t n)
+    {
+      obj* q = (obj*) p;
+      obj* volatile* my_free_list;
+      // 大于 128 调用第一级配置器
+      if (n > (size_t)__MAX_BYTES) {
+        malloc_alloc::deallocate(p, n);
+        return;
+      }
+      // 寻找对应链表。进行回收
+      my_free_list = free_list + FREELIST_INDEX(n);
+      q -> free_list_link = *my_free_list;
+      *my_free_list = q;
+    }
     static void* reallocate(void* p, size_t old_sz, size_t new_sz);
   };
+
 
 // static data member 的定义与初值设定
 template <bool threads, int inst>
@@ -207,13 +247,103 @@ template <bool threads, int inst>
   size_t __default_alloc_template<threads, inst>::heap_size \
   = 0;
 template <bool threads, int inst>
-  __default_alloc_template<threads, inst>::obj* volatile
+  typename __default_alloc_template<threads, inst>::obj* volatile
   __default_alloc_template<threads, inst>::free_list[__NFREELISTS] \
   = {0, 0, 0, 0, 
      0, 0, 0, 0, 
      0, 0, 0, 0, 
      0, 0, 0, 0};
 
+
+template <bool threads, int inst>
+  void* __default_alloc_template<threads, inst>::refill(size_t n)
+  {
+    // 默认尝试扩充 nobjs=20 个
+    int nobjs = 20;
+    // chunk_alloc 参数 nobjs 是 pass by reference
+    // 其后 nobjs 的值可能小于 20
+    char* chunk = chunk_alloc(n, nobjs);
+
+    obj* volatile* my_free_list;
+    obj* result;
+    obj* current_obj, * next_obj;
+    int i;
+
+    // 如果 nobjs=1，将区块分配给调用者
+    if (1 == nobjs) return chunk;
+    // 否则调整自由链表，纳入新节点
+    my_free_list = free_list + FREELIST_INDEX(n);
+    result = (obj*)chunk; //第一块返回给调用者
+    *my_free_list = next_obj = (obj*)(chunk + n);
+    for (i=1;  ; ++i) { //从 1 开始，因为第 0 个返回给调用者
+      current_obj -> next_obj;
+      next_obj = (obj*)((char*)next_obj + n);
+      if (nobjs - 1 == i) {
+        chrrent_obj -> free_list_link = 0;
+        break;
+      } else {
+        current_obj -> free_list_link = next_obj;
+      }
+    }
+    return result;
+  }
+
+// 从内存池中取空间给自由链表使用
+template <bool threads, int inst>
+  char* __default_alloc_template<threads, inst>::chunk_alloc(size_t size, int& nobjs)
+  {
+    char* result;
+    size_t total_bytes = size * nobjs;
+    size_t bytes_left = end_free - start_free; // 内存池剩余空间
+
+    if (bytes_left >= total_bytes) { // 内存池空间完全满足需求
+      result = start_free;
+      start_free += total_bytes;
+      return result;
+    } else if (bytes_left >= size) { // 内存池不能满足需求，但能供应一个以上的区块
+      nobjs = bytes_left / size;
+      total_bytes = size * nobjs;
+      result = start_free;
+      start_free += total_bytes;
+      return result;
+    } else { // 内存池连一个区块大小都无法提供
+      size_t bytes_to_get = 2 * total_bytes + ROUND_UP(heap_size >> 4);
+      // 先将内存池剩余空间配给合适的链表
+      if (bytes_left > 0) {
+        obj* volatile* my_free_list = free_list + FREELIST_INDEX(bytes_left);
+        (obj*)start_free -> free_list_link = *my_free_list;
+        *my_free_list = (obj*)start_free;
+      }
+      // 配置 heap 空间，补充内存池
+      start_free = (char*)malloc(bytes_to_get);
+      if (0 == start_free) { // heap 空间不足，配置失败
+        int i;
+        obj* volatile* my_free_list, * p;
+        // 优先检视所有区块足够大的链表，并将其重新划分到当前链表，
+        // 并不配置较小区块。在多线程(multi-process)机器上会导致灾难。
+        for (i=size; i<=__MAX_BYTES; i+=__ALIGN) {
+          my_free_list = free_list + FREELIST_INDEX(i);
+          p = *my_free_list;
+          if (0 != p) { // 链表有未用区块
+            *my_free_list = p -> free_list_link;
+            start_free = (char*)p;
+            end_free = start_free + i;
+            // 递归调用自己，修正 nobjs
+            return chunk_alloc(size, nobjs);
+            // 所有大于本区块的空闲链表区块都将会被编入本区块。
+          }
+        }
+        // 出现意外，完全没有内存
+        // 调用第一级配置器，向 out-of-memory 机制寻求帮助。
+        end_free = 0;
+        start_free = (char*)malloc_alloc::allocate(bytes_to_get);
+      }
+      heap_size += bytes_to_get;
+      end_free = start_free + bytes_to_get;
+      // 递归调用自己，修正 nobjs
+      return chunk_alloc(size, nobjs);
+    }
+  }
 
 } // namespace tinystl
 
